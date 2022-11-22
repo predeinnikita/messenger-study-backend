cce11292ba084810e1e7419f4af64044c0691db9
n.predein@profitbase.ru <n.predein@profitbase.ru>
2022-11-22 14:17:58 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: {credentials: true, origin: 'http://localhost:3001'} });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap(); 
