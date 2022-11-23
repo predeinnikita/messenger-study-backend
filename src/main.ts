1c586659afd3541c27a5e44a9e4472887b7b9e5f
predeinnikita <predein697@gmail.com>
2022-11-23 17:22:07 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: {credentials: true, origin: 'http://messenger-client-rtf.herokuapp.com'} });
+  const app = await NestFactory.create(AppModule, { cors: {credentials: true, origin: 'https://messenger-client-rtf.herokuapp.com'} });
 
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
